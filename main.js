226c540edc25f6a049e768564d1e8c0b8f95a034
gabfid3 <gabfid3@users.noreply.github.com>
2016-06-20 13:22:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 var letterColors = [red, orange, green, blue, purple];
-var bub = [1]
+var bub = [0]
 drawName(myName, letterColors);
 
 if(1 > (bub))
